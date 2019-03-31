4cf0540d3a6f9c188256ef79230707e29c297804
Benjamin Dudok <BenjaminWD@gmail.com>
2019-03-31 21:25:22 +0200

    Prerender pages dynamically too

--- tasks/build.prerender.js
+++ tasks/build.prerender.js
@@ -9,17 +9,24 @@ module.exports = (api, options) =>
             accessToken: process.env.VUE_APP_CONTENTFUL_API_KEY
         });
 
-        const entries = await client.getEntries({
-            content_type: 'post',
-            select: 'fields.title'
+        const pages = await client.getEntries({
+            content_type: 'page',
+            select: 'fields.slug'
         });
+
+        const posts = await client.getEntries({
+            content_type: 'post',
+            select: 'fields.slug'
+        });
+
+        console.log(pages, posts);
 
         options.pluginOptions = {
             prerenderSpa: {
                 renderRoutes: [
                     '/',
-                    '/about',
-                    ...entries.items.map((i) => `/page/${i.sys.id}`)
+                    ...pages.items.map((i) => `/${i.fields.slug}`),
+                    ...posts.items.map((i) => `/articles/${i.fields.slug}`)
                 ],
                 useRenderEvent: true,
                 onlyProduction: true
