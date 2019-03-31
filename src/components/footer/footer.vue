0958638e9875c33cac554a2966483ee0ca803fe6
Benjamin Dudok <BenjaminWD@gmail.com>
2019-03-31 15:31:16 +0200

    Added footer links

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -2,14 +2,12 @@
     <footer class="footer">
         <ul class="footer__list">
             <li class="footer__item">
-                <a href="http://www.github.com" target="_blank" class="footer__link">Github</a>
+                <a href="https://github.com/Benjamindudok" target="_blank" class="footer__link">Github</a>
             </li>
             <li class="footer__item">
-                <a href="http://www.linkedin.com" target="_blank" class="footer__link">Linkedin</a>
+                <a href="https://www.linkedin.com/in/benjaminwdudok/" target="_blank" class="footer__link">Linkedin</a>
             </li>
         </ul>
-
-        <p class="footer__text">This website is being hosted on a raspberry pi cluster</p>
     </footer>
 </template>
 
